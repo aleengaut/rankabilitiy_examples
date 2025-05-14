2527aae2a47f23caf2d48611a3f01cdd5244c7b0
Alexandre José Ferreira <144146436+aleengaut@users.noreply.github.com>
2025-05-13 22:55:24 -0300

    Update rankability_examples.py

--- rankability_examples.py
+++ rankability_examples.py
@@ -59,8 +59,14 @@ def domDeltaik2(data):
     for a in range(n):
         for b in range(n):
             test = 0
+            flag1=True
+            flag2=True
             for c in range(m):
-                if datam[a,c] > datam[b,c]: test=test+1 
+                if datam[a,c] > datam[b,c]: test=test+1
+                if data[a,c] < data[b,c] and flag2: 
+                    flag1=False
+                    flag2=False
+            if flag1 and test>0: test=m
             test = test // m
             l[a] = l[a] + test
     
@@ -77,7 +83,8 @@ def rho(data):
     rank = np.flip(rank)
 
     # dominance vector e
-    e = domDeltaik(data)[2]
+    #e = domDeltaik(data)[2]
+    e = domDeltaik2(data)
 
     rho = e.sum()/rank.sum()
     
@@ -87,13 +94,14 @@ def rho(data):
 #d = {'col1': [5, 4, 3], 'col2': [4, 6, 3]}
 #d = {'col1': [1, 2, 3, 4, 5, 6], 'col2': [6, 5, 4, 3, 2, 1]}
 #d = {'col1': [5, 3, 4, 2, 1, 0], 'col2': [4, 4, 4, 2, 1, 0]} #
-#d = {'col1': [5, 4, 3, 2, 1, 0], 'col2': [0, 1, 2, 3, 4, 5]}
+d = {'col1': [5, 4, 3, 2, 1, 0], 'col2': [0, 1, 2, 3, 4, 5]}
+d = {'col1': [5, 4, 3, 2, 1, 0], 'col2': [0, 1, 2, 3, 4, 5], 'col3': [5, 4, 3, 2, 1, 0]}
 #d = {'col1': [8, 7, 7, 5, 5, 5, 5], 'col2': [6, 6, 6, 6, 6, 6, 6]}
 #d = {'col1': [5, 5, 5, 5, 5, 5], 'col2': [5, 5, 5, 5, 5, 5]} #
 #d = {'col1': [5, 4, 3, 2, 1, 0], 'col2': [5, 4, 3, 2, 1, 0]} #
 #d = {'col1': [5, 4, 3, 2, 1, 0], 'col2': [0, 0, 0, 0, 0, 0]} #
 #d = {'col1': [5, 0, 0, 0, 0, 0], 'col2': [0, 5, 0, 0, 0, 0]} #
-d = {'col1': [5, 4, 3, 2, 1, 0], 'col2': [3, 3, 3, 2, 1, 0]} #
+#d = {'col1': [5, 4, 3, 2, 1, 0], 'col2': [3, 3, 3, 2, 1, 0]} #
 #d = {'col1': [1, 6, 4, 3, 2, 5], 'col2': [6, 1, 2, 5, 4, 3]} #
 #d = {'col1': [5,0,1,4,3,2], 'col2': [3,3,3,2,1,0]} #
 #d = {'col1': [0,5,3,2,1,4], 'col2': [5,0,1,4,3,2]} #
@@ -103,11 +111,22 @@ d = {'col1': [5, 4, 3, 2, 1, 0], 'col2': [3, 3, 3, 2, 1, 0]} #
 #d = {'col1': [1, 4, 3], 'col2': [2, 3, 2], 'col3': [3, 2, 4], 'col4': [4, 1, 1]} #
 #d = {'col1': [1, 4, 3], 'col2': [2, 3, 2], 'col3': [3, 4, 4], 'col4': [4, 5, 1]} #
 
+#d = {'col1': [5, 4, 3, 2, 1, 0], 'col2': [4, 4, 4, 2, 1, 0]} #
+#d = {'col1': [2, 2, 2, 0, 0, 0, 0], 'col2': [5, 4, 4, 4, 4, 4, 4]}
+#d = {'col1': [5, 5, 5, 5, 5, 5], 'col2': [0, 0, 0, 0, 0, 0]} #
+#d = {'col1': [1, 1, 1, 1, 1, 1], 'col2': [4, 4, 4, 4, 4, 4]} #
+#d = {'col1': [2, 0, 3, 2, 3, 1], 'col2': [4, 2, 4, 3, 3, 4]} #
+#d = {'col1': [3, 2, 0, 3, 2, 4], 'col2': [2, 1, 3, 4, 3, 3]} #
+#d = {'col1': [3, 1, 0, 2, 1, 0], 'col2': [5, 4, 3, 4, 4, 3]} #
+#d = {'col1': [3, 2, 0, 2, 2, 3], 'col2': [3, 2, 3, 4, 3, 3]} #
+#d = {'col1': [1, 1, 2, 1, 1, 2], 'col2': [3, 4, 4, 3, 4, 4]} #
+
 df = pd.DataFrame(data=d)
 data = df.copy().to_numpy()
 
 df['d']=domDeltaik(data)[1]
-df['e']=domDeltaik(data)[2]
+#df['e']=domDeltaik(data)[2]
 print(df)
 print('rho:', rho(data))
 print('e:', domDeltaik2(data))
+print('D:', domDeltaik(data)[0])
